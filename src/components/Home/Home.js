68d9520f60aca4d5e586203c3e8b9a719fe1ff68
User <user@Users-MacBook-Pro.local>
2019-06-24 10:07:02 +0100

    Home Page Component set up

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {
+    articles: []
+  };
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
