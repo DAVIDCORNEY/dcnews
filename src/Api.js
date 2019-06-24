f0674c305d17c1725f06a5c99960e9795763b9e1
User <user@Users-MacBook-Pro.local>
2019-06-24 11:22:19 +0100

    functionality added to render 10 of the latest articles to the home page

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
 const baseUrl = "https://dc-news.herokuapp.com/api";
+
+export const getArticles = query => {
+  return axios
+    .get(`${baseUrl}/articles/${query}`)
+    .then(({ data: { articles } }) => {
+      return articles;
+    });
+};
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { getArticles } from "../../Api";
 
 class Home extends Component {
   state = {
     articles: []
   };
+
+  componentDidMount() {
+    getArticles("?limit=10").then(articles => {
+      this.setState({
+        articles
+      });
+    });
+  }
+
   render() {
+    const { articles } = this.state;
     return (
       <div>
         <h1>Home Page</h1>
+        <h2>Latest Articles</h2>
+        <ul>
+          {articles.map(article => {
+            const { title, author, comment_count, votes, created_at } = article;
+            return (
+              <div key={article.article_id}>
+                {/* <Link to={`/articles/${article.article_id}`}> */}
+                <h2>{title}</h2>
+                {/* </Link> */}
+                <h3>{author}</h3>
+                <h3>{comment_count}</h3>
+                <h3>Votes {votes}</h3>
+                <h3>{created_at}</h3>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     );
   }
