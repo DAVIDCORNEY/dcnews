4da3136ac09f36f7c6464bda1e6b56ea97e0f0c8
User <user@Users-MacBook-Pro.local>
2019-06-28 09:04:01 +0100

    Loading Spinner added to Home Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
 import * as api from "../../Api";
 import ArticleCard from "../ArticleCard/ArticleCard";
+import Error from "../Error/Error";
 
 class Home extends Component {
   state = {
-    articles: []
+    articles: [],
+    error: null,
+    isLoading: true
   };
 
   componentDidMount() {
-    api.getArticles({ limit: 10 }).then(articles => {
-      this.setState({
-        articles
+    api
+      .getArticles({ limit: 10 })
+      .then(articles => {
+        this.setState({
+          articles,
+          isLoading: false,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err,
+          isLoading: false
+        });
       });
-    });
   }
 
   render() {
-    const { articles } = this.state;
+    const { articles, error, isLoading } = this.state;
+    if (isLoading) return <div className="spinner" />;
+    if (error) return <Error error={error} />;
     return (
       <div>
         <h1>Home Page</h1>
