e666b00145e2b055f4354ef74622377c776fc1f8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-09 19:57:47 +0100

    remove commented out code from nav

--- src/components/Naviagtion/Navigation.js
+++ src/components/Naviagtion/Navigation.js
@@ -77,35 +77,3 @@ const Navigation = ({ isLoggedIn, getLoggedInUser }) => {
 };
 
 export default Navigation;
-
-// import React from "react";
-// import { Link } from "@reach/router";
-
-// const Navigation = () => {
-//   return (
-//     <nav>
-//       <ul>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/about">About</Link>
-//         </li>
-//         <li>
-//           <Link to="/articles">Articles</Link>
-//         </li>
-//         <li>
-//           <Link to="/topics/coding">Coding</Link>
-//         </li>
-//         <li>
-//           <Link to="/topics/cooking">Cooking</Link>
-//         </li>
-//         <li>
-//           <Link to="/topics/football">Football</Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navigation;
